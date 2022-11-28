20dfd9c4c349685cdebf97c3e30fee9066c9d3a3
George Niotis <geoniotis@yahoo.com>
2022-11-28 23:23:06 +0200

    feat: adding hooks

---
+++ src/data/hooks.js
@@ -0,0 +1,41 @@
+import {writable,derived} from 'svelte/store';
+import {afterUpdate, onDestroy} from 'svelte';
+
+export function useState(initialState) {
+    const state = writable(initialState);
+  
+    const update = (val) =>
+      state.update((currentState) => (typeof val === 'function' ? val(currentState) : val));
+  
+    const readableState = derived(state, ($state) => $state);
+  
+    return [readableState, update];
+  }
+
+
+export function useEffect(cb, deps) {
+	let cleanup;
+	
+	function apply() {
+		if (cleanup) cleanup();
+		cleanup = cb();
+	}
+	
+	if (deps) {
+		let values = [];
+		afterUpdate(() => {
+			const new_values = deps();
+			if (new_values.some((value, i) => value !== values[i])) {
+				apply();
+				values = new_values;
+			}
+		});
+	} else {
+		// no deps = always run
+		afterUpdate(apply);
+	}
+	
+	onDestroy(() => {
+		if (cleanup) cleanup();
+	});
+}

a0b3bee23ff0eef0af1d6750cd4f7556c26ad0ca
George Niotis <geoniotis@yahoo.com>
2022-11-28 23:24:15 +0200

    feat: adding form component

---
+++ src/view/components/Login/Login.svelte
@@ -0,0 +1,68 @@
+<script>
+  import { web3, user, firebase } from "../../../data/store";
+  import { useState, useEffect } from "../../../data/hooks";
+  import LoginForm from "./LoginForm.svelte"
+  import "./style.css";
+
+  const [logedIn, setLogedIn] = useState(false)
+
+  web3.subscribe(async (_web3) => {
+    const web3 = await _web3;
+
+    if (web3) {
+      publicKey = web3.wallet.publicKey.toBase58();
+    }
+  });
+
+  firebase.onUserChanged((currentUser) => {
+    user.update(($user) => {
+      $user = currentUser;
+      return $user;
+    });
+  });
+
+  const run = async () => {
+    let userObj = await user.get()
+
+    if(userObj){
+      setLogedIn(true)
+    }
+  }
+  
+  useEffect(() => {
+    run()
+	});
+
+  firebase.onUserChanged((currentUser) => {
+    user.update(($user) => {
+      $user = currentUser;
+      return $user;
+    });
+  });
+
+  export let publicKey = "";
+</script>
+
+<div class="root">
+  <div class="backgroundColor" />
+  <div class="container innerContainer">
+    <div class="container directionContainer">
+      <div class="item">
+        <h2 classes="welcomeText">
+          <strong>WELCOME</strong> BACK!
+        </h2>
+      </div>
+      <div class="container">
+        <div item xs={12} mt={6}>
+          <h8>
+            Ticketland is a ticketing and invitation cards platform and
+            infrastructure powered by blockchain and NFT technologies.
+          </h8>
+        </div>
+      </div>
+      {#if !logedIn}
+        <LoginForm/>
+      {/if}
+    </div>
+  </div>
+</div>
---
+++ src/view/components/Login/LoginForm.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { web3, user, firebase } from "../../../data/store";
+  import { useState, useEffect } from "../../../data/hooks";
+  import "./style.css";
+
+
+  const login = async (provider) => {
+    try {
+      if(provider==='google'){
+        await firebase.signInWithGoogle();
+      }else{
+        await firebase.signInWithFacebook();
+      }
+    } catch (error) {
+      console.log(">>>>>>>>>", error);
+    }
+  };
+
+</script>
+
+<div class="container loginForm">
+  <div class='container socialMediaItem'>
+    <div class='socialMediaText'>
+      Sign in with social media
+    </div>
+  </div>
+  <div class='container iconContainer'>
+    <div class='container firstIconItem'>
+      <button class='button' on:click={async()=>{await login('google')}}>
+        <img src="assets/googleIcon.png" />
+      </button>
+    </div>
+    <div class='container secondIconItem'>
+      <button class='button' on:click={async()=>{await login('facebook')}}>
+        <img src='assets/facebookIcon.png' />
+      </button>
+    </div>
+  </div>
+</div>

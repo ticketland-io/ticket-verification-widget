c53069f10c74ebdc55d1dad47672fac1f67decd9
George Niotis <geoniotis@yahoo.com>
2022-12-01 00:20:11 +0200

    fix: code style

--- src/view/components/Login/Login.svelte
+++ src/view/components/Login/Login.svelte
@@ -1,9 +1,10 @@
 <script>
-  import {web3, user, firebase} from "../../../data/store";
-  import {useState, useEffect} from "../../../data/hooks";
-  import LoginForm from "./LoginForm.svelte";
-  import "./styles.css";
+  import {web3, user, firebase} from '../../../data/store';
+  import {useState, useEffect} from '../../../data/hooks';
+  import LoginForm from './LoginForm.svelte';
+  import './styles.css';
 
+  export let publicKey = '';
   const [logedIn, setLogedIn] = useState(false);
 
   web3.subscribe(async (_web3) => {
@@ -22,7 +23,6 @@
   });
 
   const run = async () => {
-    // await firebase.signOutUser()
     let userObj = await user.get();
 
     if (userObj) {
@@ -40,20 +40,18 @@
       return $user;
     });
   });
-
-  export let publicKey = "";
 </script>
 
-<div class="root">
-  <div class="backgroundColor" />
-  <div class="container innerContainer">
-    <div class="container directionContainer">
-      <div class="item">
-        <h2 classes="welcomeText">
+<div class='root'>
+  <div class='backgroundColor' />
+  <div class='container innerContainer'>
+    <div class='container directionContainer'>
+      <div class='item'>
+        <h2 classes='welcomeText'>
           <strong>WELCOME</strong> BACK!
         </h2>
       </div>
-      <div class="container">
+      <div class='container'>
         <div item xs={12} mt={6}>
           <h8>
             Ticketland is a ticketing and invitation cards platform and
--- src/view/components/Login/LoginForm.svelte
+++ src/view/components/Login/LoginForm.svelte
@@ -1,22 +1,22 @@
 <script>
-  import {firebase} from "../../../data/store";
-  import "./styles.css";
+  import {firebase} from '../../../data/store';
+  import './styles.css';
 
   const login = async (provider) => {
     try {
-      if(provider==='google'){
+      if(provider === 'google'){
         await firebase.signInWithGoogle();
       }else{
         await firebase.signInWithFacebook();
       }
     } catch (error) {
-      console.log(">>>>>>>>>", error);
+      console.log('>>>>>>>>>', error);
     }
   };
 
 </script>
 
-<div class="container loginForm">
+<div class='container loginForm'>
   <div class='container socialMediaItem'>
     <div class='socialMediaText'>
       Sign in with social media
@@ -25,7 +25,7 @@
   <div class='container iconContainer'>
     <div class='container firstIconItem'>
       <button class='iconButton' on:click={async()=>{await login('google')}}>
-        <img src="assets/googleIcon.png" alt='googleIcon'/>
+        <img src='assets/googleIcon.png' alt='googleIcon'/>
       </button>
     </div>
     <div class='container secondIconItem'>

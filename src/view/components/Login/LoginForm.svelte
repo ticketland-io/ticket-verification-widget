0490cc0d008cf2c7cd78b7b397ffbc0d026e1131
George Niotis <geoniotis@yahoo.com>
2022-12-30 10:37:08 +0200

    fix: double arrow function

--- src/view/components/Login/LoginForm.svelte
+++ src/view/components/Login/LoginForm.svelte
@@ -1,17 +1,18 @@
 <script>
-  import {firebase} from '../../../data/store';
-  import './styles.css';
+  import {firebase} from "../../../data/store";
+  import "./styles.css";
 
-  const login = async (provider) => {
+  const login = provider => async () => {
     try {
+      console.log(provider)
       switch (provider) {
-        case 'google':
+        case "google":
           await firebase.signInWithGoogle();
           break;
-        case 'facebook':
+        case "facebook":
           await firebase.signInWithFacebook();
           break;
-        case 'twitter':
+        case "twitter":
         default:
           await firebase.signInWithTwitter();
           break;
@@ -20,29 +21,28 @@
       //ignore
     }
   };
-
 </script>
 
-<div class='container loginForm'>
-  <div class='container socialMediaItem'>
-    <div class='socialMediaText'>
+<div class="container loginForm">
+  <div class="container socialMediaItem">
+    <div class="socialMediaText">
       Sign in with social media
     </div>
   </div>
-  <div class='container iconContainer'>
-    <div class='container iconItem'>
-      <button class='iconButton' on:click={async()=>{await login('google')}}>
-        <img src='assets/googleIcon.png' alt='googleIcon'/>
+  <div class="container iconContainer">
+    <div class="container iconItem">
+      <button class="iconButton" on:click={login("google")} >
+        <img src="assets/googleIcon.png" alt="googleIcon" />
       </button>
     </div>
-    <div class='container iconItem'>
-      <button class='iconButton' on:click={async()=>{await login('facebook')}}>
-        <img src='assets/facebookIcon.png' alt='facebookIcon'/>
+    <div class="container iconItem">
+      <button class="iconButton" on:click={login("facebook")}>
+        <img src="assets/facebookIcon.png" alt="facebookIcon" />
       </button>
     </div>
-    <div class='container iconItem'>
-      <button class='iconButton' on:click={async()=>{await login('twitter')}}>
-        <img src='assets/twitterIcon.png' alt='twitterIcon'/>
+    <div class="container iconItem">
+      <button class="iconButton" on:click={login("twitter")}>
+        <img src="assets/twitterIcon.png" alt="twitterIcon" />
       </button>
     </div>
   </div>

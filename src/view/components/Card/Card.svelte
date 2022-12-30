67607b73f77579f4553fe06ebadb7e77ba1263d9
George Niotis <geoniotis@yahoo.com>
2022-12-30 11:26:40 +0200

    fix: remove hooks

--- src/view/components/Card/Card.svelte
+++ src/view/components/Card/Card.svelte
@@ -3,28 +3,24 @@
   import {onMount} from 'svelte';
   import {format} from 'date-fns';
   import {qs, firebase} from '../../../data/store';
-  import {useState, useEffect} from '../../../data/hooks';
   import {fetchEvent, fetchMetadata, get_event_ticket_image_path} from '../../../services/s3';
   import Details from './Details.svelte';
   import './styles.css';
 
   export let ticket;
-  const [event, setEvent] = useState();
   let nftMetadata;
+  let event
+
+  const getEvent = async () => {
+    const {result} = await fetchEvent(firebase, get(qs).eventId)
+    event=result[0]
+  }
 
   onMount(async () => {
     const _qs = get(qs);
     nftMetadata = await fetchMetadata(_qs.eventId);
+    await getEvent()
   });
-
-  const getEvent = async () => {
-    const {result} = await fetchEvent(firebase, get(qs).eventId)
-    setEvent(result[0])
-  }
-
-  useEffect(() => {
-      getEvent();
-  }, () => [nftMetadata, event]);
 </script>
 
 <div class='shadow'>
@@ -34,10 +30,15 @@
         height=220px
         width=100%
         class='imageEvent'
-        src={get_event_ticket_image_path($event?.event_id, $event?.file_type)}
+        src={get_event_ticket_image_path(event?.event_id, event?.file_type)}
         alt='eventImage'
       />
     </div>
-    <Details eventName={nftMetadata?.name} startDate={format($event?.start_date || 0, 'dd.MM.yy')} seatName={ticket?.seat_name} ticketMetadata={ticket?.ticket_metadata}/>
+    <Details
+      eventName={nftMetadata?.name}
+      startDate={format(event?.start_date || 0, 'dd.MM.yy')} 
+      seatName={ticket?.seat_name} 
+      ticketMetadata={ticket?.ticket_metadata}
+    />
   </div>
 </div>

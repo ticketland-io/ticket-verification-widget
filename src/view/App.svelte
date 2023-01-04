62cffc5e819fa58f77b3eeea6e0c4fdb4e84960d
George Niotis <geoniotis@yahoo.com>
2023-01-04 16:03:20 +0200

    feat: adding event listener

--- src/view/App.svelte
+++ src/view/App.svelte
@@ -1,20 +1,26 @@
 <script>
-	import {onMount} from 'svelte'
-	import {initConnection} from '../services/connection'
-	import {connection} from '../data/store'
-	import Login from "./components/Login/Login.svelte"
-	import Tickets from './components/Tickets/Tickets.svelte'
-	import './styles.css'
+  import {onMount} from "svelte";
+  import {initConnection} from "../services/connection";
+  import {connection, originJWT} from "../data/store";
+  import Login from "./components/Login/Login.svelte";
+  import Tickets from "./components/Tickets/Tickets.svelte";
+  import "./styles.css";
 
-	onMount(async () => {
-		connection.update($connection => {
-			$connection = initConnection(process.env.CLUSTER_ENDPOINT)
-			return $connection
-		})
-	})
+  window.addEventListener("message", (event) => {
+    if (event.origin === process.env.MAIN_DAPP_ORIGIN) {
+      originJWT.set(event.data);
+    }
+  });
+
+  onMount(async () => {
+    connection.update($connection => {
+      $connection = initConnection(process.env.CLUSTER_ENDPOINT);
+      return $connection;
+    });
+  });
 </script>
 
 <main>
-	<Login/>
-	<Tickets/>
+  <Login />
+  <Tickets />
 </main>
